49750065bdbbaa0356b18effb1fb8b287933160c
Christian VIllamer <36148483+cvillamer13@users.noreply.github.com>
2023-05-03 13:27:52 +0800

    changed

--- API/v1/app/init.php
+++ API/v1/app/init.php
@@ -2,7 +2,5 @@
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
-include_once("/home/LPL-TEST/Webroot20220805/Config.php");
-include_once("/home/LPL-TEST/Webroot20220805/Functions.php");
-include_once("/home/LPL-TEST/Webroot20220805/Common.Functions.LPL.php");
-include_once("/home/LPL-TEST/Webroot20220805/CampusNet/Common.Functions.Faith.php");
+include_once('core/Config.php");
+
